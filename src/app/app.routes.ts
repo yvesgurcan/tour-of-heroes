4d11d5512110ff8b3f3e5d1847b5f816f2f2e075
Yves Gurcan <gurcan.yves+github@gmail.com>
2023-11-17 12:04:53 -0800

    Tutorial all the way to https://angular.io/tutorial/tour-of-heroes/toh-pt5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
